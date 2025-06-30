525538bca767daef8f9b8786f32e643164764e3c
HuongLe <huong.le@asnet.com.vn>
2025-06-30 17:22:23 +0700

    Update test cases for login feature

--- PocketBase/e2e/fixtures/pageFixtures.ts
+++ PocketBase/e2e/fixtures/pageFixtures.ts
@@ -1,14 +1,20 @@
 import { test as base, expect } from '@playwright/test';
-import { LoginPage } from '../pages';
+import { DashboardPage, LoginPage } from '../pages';
 
 interface PagesFixture {
   loginPage: LoginPage;
+  dashboardPage: DashboardPage;
 }
 
 const test = base.extend<PagesFixture>({
   loginPage: async ({ page }, use) => {
     const login = new LoginPage(page);
     await use(login);
+  },
+
+  dashboardPage: async ({ page }, use) => {
+    const dashboardPage = new DashboardPage(page);
+    await use(dashboardPage);
   },
 });
 
