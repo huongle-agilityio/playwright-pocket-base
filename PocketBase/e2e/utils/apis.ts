512bfdd17abbe8eac2ea50cba8c73056c771a812
HuongLe <huong.le@asnet.com.vn>
2025-07-03 09:22:26 +0700

    Add extractAccessToken function to retrieve access token from auth file

---
+++ PocketBase/e2e/utils/apis.ts
@@ -0,0 +1,37 @@
+import * as path from 'path';
+import * as fs from 'fs';
+const authFilePath = path.resolve(__dirname, '../../playwright/.auth/user.json');
+
+/**
+ * Extracts the access token from the auth file stored at the path specified by
+ * the `authFilePath` constant. The token is extracted from the `__pb_superuser_auth__`
+ * local storage item of the origin specified by the `BASE_URL` environment variable.
+ * If the token is not found, an empty string is returned.
+ *
+ * @returns {string} The access token or an empty string if the token is not found.
+ */
+export const extractAccessToken = () => {
+  try {
+    if (!fs.existsSync(authFilePath)) {
+      return;
+    }
+
+    const authData = JSON.parse(fs.readFileSync(authFilePath, 'utf-8'));
+
+    const origin = authData.origins.find(
+      (o: { origin: string }) => o.origin === process.env.BASE_URL,
+    );
+    const authItem = origin?.localStorage.find(
+      (item: { name: string; value: string }) => item.name === '__pb_superuser_auth__',
+    );
+
+    if (authItem?.value) {
+      const parsed = JSON.parse(authItem.value);
+      return parsed.token || '';
+    }
+  } catch (err) {
+    console.warn('Could not extract token', err);
+  }
+
+  return '';
+};
