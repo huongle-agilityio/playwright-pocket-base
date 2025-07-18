2e67c95378b2b2c96b634df3a49b94026cbe2cc8
HuongLe <huong.le@asnet.com.vn>
2025-07-18 17:05:25 +0700

    Add comments for scenario login test

--- PocketBase/e2e/tests/steps/login.ts
+++ PocketBase/e2e/tests/steps/login.ts
@@ -20,6 +20,7 @@ Given('I am on the login page', async ({ loginPage }) => {
   await loginPage.goto();
 });
 
+// Scenario: Success with valid inputs
 When('I submit the login form with correct credentials', async ({ loginPage, page }) => {
   const responsePromise = waitForPostResponse({ url: API_URLS.LOGIN, page });
   await loginPage.form.loginAs(USER.USER_NAME, USER.PASSWORD);
@@ -38,6 +39,7 @@ Then('I should receive a valid token and correct email in response', async () =>
   expect(responseBody.record.email).toBe(USER.USER_NAME);
 });
 
+// Scenario: Failure with invalid "<field>"
 const INVALID_CASES = {
   username: { email: USER.INVALID_USER_NAME, password: USER.PASSWORD },
   password: { email: USER.USER_NAME, password: USER.INVALID_PASSWORD },
@@ -64,6 +66,7 @@ Then('the response should contain message "Failed to authenticate."', async () =
   expect(responseBody.message).toBe(MESSAGES.FAILED_TO_AUTHENTICATE);
 });
 
+// Scenario: With empty inputs
 When('I submit the login form with empty inputs', async ({ loginPage }) => {
   await loginPage.form.loginAs('', '');
 });
