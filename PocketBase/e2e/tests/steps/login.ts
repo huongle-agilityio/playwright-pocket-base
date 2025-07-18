2e875635bd96fc910039a0c4b7a2e04a8d4c70a8
HuongLe <huong.le@asnet.com.vn>
2025-07-18 16:47:48 +0700

    #56 Implement bdd for feature login

---
+++ PocketBase/e2e/tests/steps/login.ts
@@ -0,0 +1,77 @@
+import { createBdd } from 'playwright-bdd';
+import { type Response } from '@playwright/test';
+import { test, expect } from '@/fixtures/noStorage';
+
+// Constants
+import { API_URLS, MESSAGES, STATUS_CODES, USER } from '@/constants';
+
+// Interfaces
+import { User } from '@/interfaces';
+
+// Utils
+import { waitForPostResponse } from '@/utils';
+
+const { Given, When, Then } = createBdd(test);
+
+let response: Response;
+let responseBody: { token: string; record: User; message: string };
+
+Given('I am on the login page', async ({ loginPage }) => {
+  await loginPage.goto();
+});
+
+When('I submit the login form with correct credentials', async ({ loginPage, page }) => {
+  const responsePromise = waitForPostResponse({ url: API_URLS.LOGIN, page });
+  await loginPage.form.loginAs(USER.USER_NAME, USER.PASSWORD);
+
+  response = await responsePromise;
+  responseBody = await response.json();
+});
+
+Then('the dashboard is loaded', async ({ dashboardPage }) => {
+  await dashboardPage.verifyDashboardLoaded();
+});
+
+Then('I should receive a valid token and correct email in response', async () => {
+  expect(response.status()).toBe(STATUS_CODES.SUCCESS);
+  expect(responseBody.token).toBeTruthy();
+  expect(responseBody.record.email).toBe(USER.USER_NAME);
+});
+
+const INVALID_CASES = {
+  username: { email: USER.INVALID_USER_NAME, password: USER.PASSWORD },
+  password: { email: USER.USER_NAME, password: USER.INVALID_PASSWORD },
+};
+
+When(
+  'I submit the login form with invalid {string} input',
+  async ({ page, loginPage }, field: 'username' | 'password') => {
+    const responsePromise = waitForPostResponse({ url: API_URLS.LOGIN, page });
+    await loginPage.form.loginAs(INVALID_CASES[field].email, INVALID_CASES[field].password);
+
+    response = await responsePromise;
+    responseBody = await response.json();
+  },
+);
+
+Then('I should see an error message "Invalid login credentials."', async ({ loginPage }) => {
+  await loginPage.verifyToastMessage(MESSAGES.INVALID_LOGIN_CREDENTIALS);
+});
+
+Then('the response should contain message "Failed to authenticate."', async () => {
+  expect(response.status()).toBe(STATUS_CODES.BAD_REQUEST);
+  expect(responseBody.token).toBeFalsy();
+  expect(responseBody.message).toBe(MESSAGES.FAILED_TO_AUTHENTICATE);
+});
+
+When('I submit the login form with empty inputs', async ({ loginPage }) => {
+  await loginPage.form.loginAs('', '');
+});
+
+Then('the email input should be invalid', async ({ loginPage }) => {
+  const isValid = await loginPage.form.email.evaluate((input: HTMLInputElement) =>
+    input.checkValidity(),
+  );
+
+  expect(isValid).toBe(false);
+});
